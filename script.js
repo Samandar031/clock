5f88e7afd86619b73ac9de852f89e0eb241246b4
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-03-19 05:21:45 +0500

    boshqattan

---
+++ script.js
@@ -0,0 +1,19 @@
+// let deg = 6;
+// let hour = document.querySelector(".hour");
+// let minut = document.querySelector(".minut");
+// let secund = document.querySelector(".secund");
+
+// setInterval(() => {
+//   let day = new Date();
+//   console.log(day);
+//   let hh = day.getHours() * 30;
+//   console.log(hh);
+//   let mn = day.getMinutes() * deg;
+//   console.log(mn);
+//   let sc = day.getSeconds() * deg;
+//   console.log(sc);
+
+//   hour.style.transform = `rotateZ(${hh + mn / 12}deg)`;
+//   secund.style.transform = `rotateZ(${sc}deg)`;
+//   minut.style.transform = `rotateZ(${mn}deg)`;
+// });
