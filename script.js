e8507b9027a9a6159da44ca6b0e0b2d325b0b404
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-03-19 19:43:11 +0500

    soat qoshildi

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let hour = document.querySelector(".hour");
 let min = document.querySelector(".min");
 let sec = document.querySelector(".sec");
 
+let timeHour = document.querySelector(".time-one");
+let timeMin = document.querySelector(".time-two");
+let timeSec = document.querySelector(".time-three");
+
 setInterval(function () {
   let deg = 6;
   let bigdeg = 30;
@@ -16,4 +20,8 @@ setInterval(function () {
   hour.style.transform = `rotateZ(${hr + mn / 12}deg)`;
   min.style.transform = `rotateZ(${mn}deg)`;
   sec.style.transform = `rotateZ(${sc}deg)`;
+
+  timeHour.textContent = soat.getHours();
+  timeMin.textContent = soat.getMinutes();
+  timeSec.textContent = soat.getSeconds();
 });
