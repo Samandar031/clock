acee8333137367c5335e7686ee1e6b916bd708a6
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-03-31 23:23:21 +0500

    rebr

--- script.js
+++ script.js
@@ -6,45 +6,64 @@ let timeHour = document.querySelector(".time-one");
 let timeMin = document.querySelector(".time-two");
 let timeSec = document.querySelector(".time-three");
 
+// setInterval(function () {
+//   let deg = 6;
+//   let bigdeg = 30;
+
+//   let soat = new Date();
+//   console.log(soat);
+
+//   let hr = soat.getHours() * bigdeg;
+//   let mn = soat.getMinutes() * deg;
+//   let sc = soat.getSeconds() * deg;
+
+//   hour.style.transform = `rotateZ(${hr + mn / 12}deg)`;
+//   min.style.transform = `rotateZ(${mn}deg)`;
+//   sec.style.transform = `rotateZ(${sc}deg)`;
+
+//   timeHour.textContent = soat.getHours();
+//   timeMin.textContent = soat.getMinutes();
+//   timeSec.textContent = soat.getSeconds();
+// });
 setInterval(function () {
-  let deg = 6;
-  let bigdeg = 30;
 
-  let soat = new Date();
-  console.log(soat);
+let deg = 6;
+let bigDeg = 30;
 
-  let hr = soat.getHours() * bigdeg;
-  let mn = soat.getMinutes() * deg;
-  let sc = soat.getSeconds() * deg;
+let kun = new Data();
+
+let soat = kun.getHours();
+let minut = kun.getMinutes();
+let secunt = kun.getSeconds();
+
+  let hr = soat;
+  let mn = minut;
+  let sc = secunt;
 
   hour.style.transform = `rotateZ(${hr + mn / 12}deg)`;
   min.style.transform = `rotateZ(${mn}deg)`;
   sec.style.transform = `rotateZ(${sc}deg)`;
-
-  timeHour.textContent = soat.getHours();
-  timeMin.textContent = soat.getMinutes();
-  timeSec.textContent = soat.getSeconds();
-});
+}, 1000);
 
 // tuggle
 
-let tuggle = document.querySelector(".tuggle");
-let btnTugle = document.querySelector(".btn-tun");
-let body = document.querySelector("body");
+// let tuggle = document.querySelector(".tuggle");
+// let btnTugle = document.querySelector(".btn-tun");
+// let body = document.querySelector("body");
 
-let a = 0;
+// let a = 0;
 
-function func() {
-  if (a == 2) {
-    a = 0;
-    document.documentElement.style.setProperty("--text", "#121145");
-  } else if (a == 1) {
-    document.documentElement.style.setProperty("--text", "black");
-  }
-  btnTugle.style.transform = `translateX(${a * 38}px)`;
-}
+// function func() {
+//   if (a == 2) {
+//     a = 0;
+//     document.documentElement.style.setProperty("--text", "#121145");
+//   } else if (a == 1) {
+//     document.documentElement.style.setProperty("--text", "black");
+//   }
+//   btnTugle.style.transform = `translateX(${a * 38}px)`;
+// }
 
-tuggle.addEventListener("click", function () {
-  a++;
-  func();
-});
+// tuggle.addEventListener("click", function () {
+//   a++;
+//   func();
+// });
